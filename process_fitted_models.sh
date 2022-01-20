604085a89d3bb68b361c4ab20eb04fc9c91ba77b
misken <isken@oakland.edu>
2022-01-20 13:14:49 -0500

    added scripts and new notebooks

---
+++ process_fitted_models.sh
@@ -0,0 +1,4 @@
+python process_fitted_models.py exp11 pp pp_results_exp11.pkl output output/figures/exp11
+python process_fitted_models.py exp11 ldr ldr_results_exp11.pkl output output/figures/exp11
+python process_fitted_models.py exp11 obs obs_results_exp11.pkl output output/figures/exp11
+
---
+++ run_mm_fits.sh
@@ -0,0 +1,3 @@
+python run_mm_fits_ldr.py
+python run_mm_fits_pp.py
+python run_mm_fits_obs.py
